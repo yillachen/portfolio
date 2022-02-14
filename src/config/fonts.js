a49149dcb4c0825662eb88129da58b8cc4ee17fd
yillachen <chn.yilla@gmail.com>
2022-02-14 15:33:44 -0500

    fix: netlify deploy fail

---
+++ src/config/fonts.js
@@ -0,0 +1,19 @@
+import Basis from "../client/fonts/BasisGrotesque.woff";
+import Basis2 from "../client/fonts/BasisGrotesque.woff2";
+import RobotoMono from "../client/fonts/RobotoMono.woff";
+import RobotoMono2 from "../client/fonts/RobotoMono.woff2";
+import Tiempos from "../client/fonts/TiemposFine.woff";
+import Tiempos2 from "../client/fonts/TiemposFine.woff2";
+import TiemposItalic from "../client/fonts/TiemposFineItalic.woff";
+import TiemposItalic2 from "../client/fonts/TiemposFineItalic.woff2";
+
+export {
+  Basis,
+  Basis2,
+  RobotoMono,
+  RobotoMono2,
+  Tiempos,
+  Tiempos2,
+  TiemposItalic,
+  TiemposItalic2
+};
